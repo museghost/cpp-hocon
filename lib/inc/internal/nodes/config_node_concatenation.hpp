88c5fc0ca927c42222002cfe1bcdf87a637c1ac5
museghost <museghost@users.noreply.github.com>
2020-05-07 15:34:49 +0900

    virtual dtor into config_node_concatenation

--- lib/inc/internal/nodes/config_node_concatenation.hpp
+++ lib/inc/internal/nodes/config_node_concatenation.hpp
@@ -7,6 +7,7 @@ namespace hocon {
     class config_node_concatenation : public config_node_complex_value {
     public:
         config_node_concatenation(shared_node_list children);
+        virtual ~config_node_concatenation() = default;
 
         std::shared_ptr<const config_node_complex_value> new_node(shared_node_list nodes) const override;
     };
