fd0613f294fb5648123487849c85811eb8a48d38
museghost <museghost@users.noreply.github.com>
2020-05-07 15:32:53 +0900

    virtual dtor into config_node_array

--- lib/inc/internal/nodes/config_node_array.hpp
+++ lib/inc/internal/nodes/config_node_array.hpp
@@ -10,6 +10,7 @@ namespace hocon {
     class config_node_array : public config_node_complex_value {
     public:
         config_node_array(shared_node_list children);
+        virtual ~config_node_array() = default;
 
         std::shared_ptr<const config_node_complex_value> new_node(shared_node_list nodes) const override;
     };
