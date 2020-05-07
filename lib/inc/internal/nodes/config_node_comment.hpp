57ac7782e82e3634f12516e4b6d89eec2305a5f7
museghost <museghost@users.noreply.github.com>
2020-05-07 15:55:45 +0900

    virtual dtor into config_node_comment

--- lib/inc/internal/nodes/config_node_comment.hpp
+++ lib/inc/internal/nodes/config_node_comment.hpp
@@ -7,6 +7,7 @@ namespace hocon {
     class config_node_comment : public config_node_single_token {
     public:
         config_node_comment(shared_token comment);
+        virtual ~config_node_comment() = default;
 
         std::string comment_text() const;
     };
